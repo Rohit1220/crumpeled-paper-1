0320640a2d3c97adb4d83da6acfa08e1a7511f9e
Rohit1220 <65952260+Rohit1220@users.noreply.github.com>
2020-06-28 11:07:51 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # MatterJSBoilerPlate
 MatterJSBoilerPlate
-index.html
-sketch.js
-p5.play.js
-p5.sound.js
--- sketch.js
+++ sketch.js
@@ -1,44 +1,35 @@
-var paper;
-var side1, side2, side3;
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 function setup() {
 	createCanvas(800, 700);
+	rectMode(CENTER);
+	packageSprite=createSprite(100,600,50,50);
+	helicopterSprite=createSprite(width/2,200,50,50);
+	groundSprite=createSprite(width/2,height-35,width,10);
+	groundSprite.shapeColor=color(255)
 	engine = Engine.create();
 	world = engine.world;
-	paper = createSprite(50, 650, 20, 20);
-	side1 = createSprite(500, 600, 10, 250);
-	side2 = createSprite(700, 600, 10, 250);
-	side3 = createSprite(600, 695, 200, 10);
+	packageBody = Bodies.circle(100,600,5,{restitution:0.5,isStatic:true});
+	World.add(world, packageBody);
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
 	Engine.run(engine);
 }
 function draw() {
-	rectMode(CENTER);
-	background(0);
-	drawSprites();
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
 }
-function keyPressed()
+function keypressed()
 {
-	if (keyCode===85)
+	if (keyCode === 32)
 	{
-		paper.velocityY=-10;
-		paper.velocityX=0;
-	}
-	if (keyCode===68)
-	{
-		paper.velocityY=10;
-		paper.velocityX=0;
-	}
-	if (keyCode===76)
-	{
-		paper.velocityX=10;
-		paper.velocityY=0;
-	}
-	if (keyCode===82)
-	{
-		paper.velocityX=-10;
-		paper.velocityY=0;
+	Matter.Body.applyForce(packageBody,225,10)
 	}
 }

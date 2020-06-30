e7cbf9f23b495524a3201d4726369951f10399e6
Rohit1220 <65952260+Rohit1220@users.noreply.github.com>
2020-06-30 15:37:17 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-# MatterJSBoilerPlate
-MatterJSBoilerPlate
-index.html
-sketch.js
-p5.play.js
-p5.sound.js
+# CrumpledPaper
--- sketch.js
+++ sketch.js
@@ -1,29 +1,40 @@
-var paper;
-var side1, side2, side3;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj, paperObject,groundObject	
+var world;
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(1600, 700);
+	rectMode(CENTER);
 	engine = Engine.create();
 	world = engine.world;
-	paper = createSprite(50, 650, 20, 20);
-	side1 = createSprite(500, 600, 10, 250);
-	side2 = createSprite(700, 600, 10, 250);
-	side3 = createSprite(600, 695, 200, 10);
-	Matter.engine.add(paper);
+	dustbinObj=new dustbin(1200,650);
+	paperObject=new paper(200,450,40);
+	groundObject=new ground(width/2,670,width,20);
+	//Create a Ground
+	var render = Render.create({
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1200,
+	    height: 700,
+	    wireframes: false
+	  }
+	});
 	Engine.run(engine);
+	//Render.run(render);
 }
 function draw() {
-	rectMode(CENTER);
-	background(0);
-	drawSprites();
+  rectMode(CENTER);
+  background(0);
+  dustbinObj.display();
+  paperObject.display();
+  groundObject.display();
 }
-function keyPressed()
-{
-	if (keyCode===85)
-	{
-		
-	}
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+  	}
 }
